bac9de96f1b2932d7a9b4ca09457018c90d5c6f3
ToveLissner <tove_lissner@hotmail.com>
2023-05-01 23:32:52 +0200

    token

--- electronics_server/src/middleware/authorization.ts
+++ electronics_server/src/middleware/authorization.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { VerifyErrors } from "jsonwebtoken";
 import { IJwtPayload } from "../models/IJwtPayload";
 
 class Authorization {
@@ -9,21 +9,22 @@ class Authorization {
     console.log(token);
 
     if (!token) {
+      jwt.verify(
+        token,
+        process.env.JWT_TOKEN as string,
+        undefined,
+        (err: VerifyErrors | null, decoded: object | undefined) => {
+          if (err) {
+            return res.status(403).json({ error: "Ogiltig token!" });
+          }
+          const payload = decoded as IJwtPayload;
+          req.body.username = payload.username;
+          next();
+        }
+      );
+    } else {
       return res.status(401).json({ error: "Token saknas" });
     }
-
-    jwt.verify(
-      token,
-      process.env.JWT_TOKEN as string,
-      (err: Error, payload: IJwtPayload) => {
-        if (err) {
-          return res.status(403).json({ error: "Ogiltig token!" });
-        }
-
-        req.username = payload.username;
-        next();
-      }
-    );
   }
 }
 
